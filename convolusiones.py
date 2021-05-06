bd8ec608591495ad41a42fa5a47d0390797ed895
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-05-06 11:31:32 -0500

    Convolusión simple

--- convolusiones.py
+++ convolusiones.py
@@ -2,19 +2,46 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
  
-def convolution(image, kernel):
+def conv_helper(fragment, kernel):
+    f_row, f_col = fragment.shape
+    k_row, k_col = kernel.shape 
+    result = 0.0
+    for row in range(f_row):
+        for col in range(f_col):
+            result += fragment[row,col] *  kernel[row,col]
+    return result
 
+def convolution(image, kernel):
+    print('Matriz')
     for row in image:
         for col in row:
             print(col ,end=' ')
         print(end='\n')
 
     print('\n')
-
+    print('Kernel')
+    
     for row in kernel:
             for col in row:
                 print(col ,end=' ')
             print(end='\n')
+
+    image_row, image_col = image.shape
+    kernel_row, kernel_col = kernel.shape
+
+    output = np.zeros(image.shape)
+
+    for row in range(image_row):
+        for col in range(image_col):
+                output[row, col] = conv_helper(
+                                    image[row:row + kernel_row, 
+                                    col:col + kernel_col],kernel)
+
+    plt.imshow(output, cmap='gray')
+    plt.title("Output Image using {}X{} Kernel".format(kernel_row, kernel_col))
+    plt.show()
+
+    return output
 
 if __name__ == '__main__':
 
@@ -28,4 +55,4 @@ if __name__ == '__main__':
                         [0,0,0],
                         [2,10,3]])
     
-    convolution(image, filter)
+    print(convolution(image, filter))
