8ea6f8c978db9c45e3056dc7a58fe06cd6771689
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-05-06 14:11:07 -0500

    Convolusión con imágen y filtro Sobel

--- convolusiones.py
+++ convolusiones.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import argparse
 import matplotlib.pyplot as plt
 
 # Función que multiplica la matriz de image con la de kernel y devuelve la suma de esta
@@ -15,23 +16,22 @@ def conv_helper(fragment, kernel):
 # Función de convolución sin padding que devuelve la matriz resultante del mismo tamaño de la matriz image
 def convolution(image, kernel):
 
-    # Impresión de matriz image
-    print('Matriz')
-    for row in image:
-        for col in row:
-            print(col ,end=' ')
-        print(end='\n')
+    if len(image.shape) == 3:
+        print("Found 3 Channels : {}".format(image.shape))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        print("Converted to Gray Channel. Size : {}".format(image.shape))
+    else:
+        print("Image Shape : {}".format(image.shape))
 
-    print('\n')
+    print("Kernel Shape : {}".format(kernel.shape))
+
+    # Impresión de matriz image
     print('Kernel')
     # Impresión de filtro kernel
     for row in kernel:
             for col in row:
                 print(col ,end=' ')
             print(end='\n')
-
-    print('\n')
-    print('Convolución válida')
 
     image_row, image_col = image.shape # obtiene tupla con las medidas de alto y ancho de la imagen
     kernel_row, kernel_col = kernel.shape # obtiene tupla con las medidas de alto y ancho el filtro
@@ -44,6 +44,10 @@ def convolution(image, kernel):
                                     image[row:row + kernel_row, 
                                     col:col + kernel_col],kernel)
     # Mostrar el plot
+    plt.imshow(image, cmap='gray')
+    plt.title("Image of {}X{}".format(image_row, image_col))
+    plt.show()
+
     plt.imshow(output, cmap='gray')
     plt.title("Output Image using {}X{} Kernel".format(kernel_row, kernel_col))
     plt.show()
@@ -52,17 +56,22 @@ def convolution(image, kernel):
 
 if __name__ == '__main__':
 
-    # Matriz de imágen
-    image = np.array([  [1,2,3,4,5,6],
-                        [7,8,9,10,11,12],
-                        [0,0,1,16,17,18],
-                        [0,1,0,7,23,24],
-                        [1,7,6,5,4,3]])
-
-    # Matriz de filtro
+    # Matriz de filtro Sobel
     filter = np.array([ [-1,0,1],
                         [-2,0,2],
                         [-1,0,1]])
-    
+
+    # obtiene la imagen de la linea de comando "python convolusiones.py -i Hatsune3.png"
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path of image")
+    args = vars(ap.parse_args())
+
+    # cambia la imagen a formato numerico, matriz de 3 dimensiones rgb
+    image = cv2.imread(args["image"])
+
+    plt.imshow(image, cmap='gray')
+    plt.title("Original Image")
+    plt.show()
+
     # Se usa el print para ver la matriz resultante
-    print(convolution(image, filter))
+    convolution(image, filter)
